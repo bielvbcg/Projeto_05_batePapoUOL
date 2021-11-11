2e92b1ff43b423c6e4684c915f392f1170719116
Gabriel Buarque <gabrielvbcallegario@gmail.com>
2021-11-11 14:38:37 -0300

    pede nome do usuario e mantem ele conectado

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,33 @@
+const url = "https://mock-api.driven.com.br/api/v4/uol"
 let ultimaMensagem = "";
+let name;
+
+function logarChat()
+{
+  name = prompt("Digite seu nome");
+  const promessa = axios.post(`${url}/participants` , {name});
+
+  promessa.then(iniciarChat);
+  promessa.catch(logarChat);
+}
+
+function iniciarChat()
+{
+  buscarMensagens();
+  manterConexao();
+
+  setInterval(buscarMensagens , 3000);
+  setInterval(manterConexao , 5000);
+}
+
+function manterConexao()
+{
+  const promessa = axios.post(`${url}/status` , {name});
+}
 
 function buscarMensagens()
 {
-  const promessa = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+  const promessa = axios.get(`${url}/messages`);
   promessa.then(popularChat);
 }
 
@@ -28,17 +53,17 @@ function popularChat(promessa)
         chat.innerHTML += `
         <div class="mensagem" data-identifier="message">
           <span>(${mensagemAtual.time}) </span>
-          <p><strong>${mensagemAtual.from}</strong> Para <strong>${mensagemAtual.to}<strong>: ${mensagemAtual.text}</p>
+          <p><strong>${mensagemAtual.from}</strong> Para <strong>${mensagemAtual.to}</strong>: ${mensagemAtual.text}</p>
         </div>`;
 
         break;
 
-      case "private-message":
+      case "private_message":
         
         chat.innerHTML += `
         <div class="mensagem privado" data-identifier="message">
           <span>(${mensagemAtual.time}) </span>
-          <p><strong>${mensagemAtual.from}</strong> reservadamente para <strong>${mensagemAtual.to}<strong> ${mensagemAtual.text}</p>
+          <p><strong>${mensagemAtual.from}</strong> reservadamente para <strong>${mensagemAtual.to}</strong> ${mensagemAtual.text}</p>
         </div>`;
 
         break;
@@ -54,6 +79,6 @@ function popularChat(promessa)
         break;
     }
   }
+  const mostrarUltimaMensagem = document.querySelector(".chat .mensagem:last-child");
+  mostrarUltimaMensagem.scrollIntoView();
 }
-
-setInterval(buscarMensagens , 3000);
